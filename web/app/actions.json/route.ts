ebd9cc2c0b4dbaf1537435b354dd4143ec1f3d15
TejasAnand <anandtejas455@gmail.com>
2024-08-17 17:02:05 -0400

    actions.json added

---
+++ web/app/actions.json/route.ts
@@ -0,0 +1,23 @@
+import { ACTIONS_CORS_HEADERS, ActionsJson } from '@solana/actions';
+
+export const GET = async () => {
+  const payload: ActionsJson = {
+    rules: [
+      // map all root level routes to an action
+      {
+        pathPattern: '/**',
+        apiPath: '/api/action',
+      },
+      // {
+      //   pathPattern: "/",
+      //   apiPath: "/api/action",
+      // },
+    ],
+  };
+
+  return Response.json(payload, {
+    headers: ACTIONS_CORS_HEADERS,
+  });
+};
+
+export const OPTIONS = GET;
